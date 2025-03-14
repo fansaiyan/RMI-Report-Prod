a53a64ffc7ef8dc15b18cd42fce277ee2cbd144e
irfan.ms <irfan.ms@allbestsistem.com>
2025-03-15 00:20:29 +0700

    SMI Survey

--- env.js
+++ env.js
@@ -3,8 +3,15 @@
 	window.__env = window.__env || {};
 
 	// API url
-	window.__env.url = 'http://train.ermi.online/';
-	window.__env.db = 'rmi';
+	window.__env.url = path + ':8069/';
+	window.__env.db = 'rmi-survey';
+	window.__env.url_smi = 'http://localhost:8070/';
+	window.__env.smi_survey_code = 'SMI-SA';
+
+	// window.__env.url = 'http://train.ermi.online/';
+	// window.__env.db = 'rmi';
+	// window.__env.url_smi = 'http://localhost:8070/';
+	// window.__env.smi_survey_code = 'SMI-SA';
 
 	// Whether or not to enable debug mode
 	// Setting this to false will disable console output
